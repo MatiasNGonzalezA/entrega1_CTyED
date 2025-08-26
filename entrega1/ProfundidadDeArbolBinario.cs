5669c8a2701441eac7e2866d32f56e8f4ae6ad84
Matias Gonzalez <matiasgonzalez@192.168.1.14>
2025-08-25 21:47:45 -0300

    entrega 1 resuelta

---
+++ entrega1/ProfundidadDeArbolBinario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace entrega1
+{
+    public class ProfundidadDeArbolBinario
+    {
+        private ArbolBinario<int> arbol;
+
+        public ProfundidadDeArbolBinario(ArbolBinario<int> arbol)
+        {
+            this.arbol = arbol;
+        }
+
+        public int sumaElementosProfundidad(int p)
+        {
+            return sumaEnNivel(arbol, 0, p);
+        }
+
+        private int sumaEnNivel(ArbolBinario<int> nodo, int nivelActual, int profundidad)
+        {
+            if (nodo == null)
+                return 0;
+
+            if (nivelActual == profundidad)
+                return nodo.getDatoRaiz();
+
+            int sumaIzquierda = sumaEnNivel(nodo.getHijoIzquierdo(), nivelActual + 1, profundidad);
+            int sumaDerecha = sumaEnNivel(nodo.getHijoDerecho(), nivelActual + 1, profundidad);
+
+            return sumaIzquierda + sumaDerecha;
+        }
+    }
+
+}
+
--- entrega1/Program.cs
+++ entrega1/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using entrega1;
 
+public class Program
+{
+    public static void Main()
+    {
+
+        ArbolBinario<int> raiz = new ArbolBinario<int>(11);
+        ArbolBinario<int> nodo6 = new ArbolBinario<int>(6);
+        ArbolBinario<int> nodo25 = new ArbolBinario<int>(25);
+        ArbolBinario<int> nodo4 = new ArbolBinario<int>(4);
+        ArbolBinario<int> nodo9 = new ArbolBinario<int>(9);
+        ArbolBinario<int> nodo35 = new ArbolBinario<int>(35);
+
+        raiz.agregarHijoIzquierdo(nodo6);
+        raiz.agregarHijoDerecho(nodo25);
+        nodo6.agregarHijoIzquierdo(nodo4);
+        nodo6.agregarHijoDerecho(nodo9);
+        nodo25.agregarHijoDerecho(nodo35);
+
+        ProfundidadDeArbolBinario profundidad = new ProfundidadDeArbolBinario(raiz);
+
+        Console.WriteLine("Suma en profundidad 0: " + profundidad.sumaElementosProfundidad(0)); // 11
+        Console.WriteLine("Suma en profundidad 1: " + profundidad.sumaElementosProfundidad(1)); // 31
+        Console.WriteLine("Suma en profundidad 2: " + profundidad.sumaElementosProfundidad(2)); // 48
+    }
+}
